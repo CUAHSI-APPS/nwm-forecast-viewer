bd28a3b468679f26a3408afecab58ccc13d6b63f
michaels <masouffront@gmail.com>
2016-06-05 08:57:46 -0600

    corrected shapefile dependency to pyshp

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ app_class = 'nwm_forecasts.app:nwmForecasts'
 app_package_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'tethysapp', app_package)
 
 ### Python Dependencies ###
-dependencies = ['netCDF4', 'shapefile']
+dependencies = ['netCDF4', 'pyshp']
 
 setup(
     name=release_package,
